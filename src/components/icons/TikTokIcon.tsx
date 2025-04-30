9236dc96eb7946e555eac4982e32e8299c29c3eb
Darkhorse <horsedark374@gmail.com>
2025-04-30 17:32:40 -0500

    icons

---
+++ src/components/icons/TikTokIcon.tsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+const TikTokIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M9 12a4 4 0 1 0 4 4V4a5 5 0 0 0 5 5" />
+  </svg>
+);
+
+export default TikTokIcon;
